0ed22923e683b1d7cf7bd549cef4f20ef3cd6f3f
vgalanov_at_franpos <vgalanov@franpos.com>
2019-05-01 13:51:03 +0300

    init

---
+++ HelloDocker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelloDocker.MvcApplication" Language="C#" %>
---
+++ HelloDocker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloDocker.Startup))]
+namespace HelloDocker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
